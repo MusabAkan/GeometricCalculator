8f0dfb016609266dccbc3ff27d509c64bca3dc9a
Musab Akan <104889820+MusabAkan@users.noreply.github.com>
2023-10-22 14:02:21 +0300

    Add project files.

---
+++ GeometricCalculator.cs
@@ -0,0 +1,54 @@
+﻿namespace GeometricCalculator
+{
+    class GeometricCalculator
+    {
+        public static double CalculateArea(string shape, double[] sides)
+        {
+            switch (shape.ToLower())
+            {
+                case "daire":
+                    double radius = sides[default(int)];
+                    return Math.PI * radius * radius;
+                case "üçgen":
+                    double a = sides[default(int)];
+                    double b = sides[1];
+                    double c = sides[2];
+                    double s = (a + b + c) / 2;
+                    return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+                case "kare":
+                    double side = sides[default(int)];
+                    return side * side;
+                case "dikdörtgen":
+                    double width = sides[default(int)];
+                    double height = sides[1];
+                    return width * height;
+                default:
+                    throw new ArgumentException("Geçersiz geometrik şekil.");
+            }
+        }
+
+        public static double CalculatePerimeter(string shape, double[] sides)
+        {
+            switch (shape.ToLower())
+            {
+                case "daire":
+                    double radius = sides[default(int)];
+                    return 2 * Math.PI * radius;
+                case "üçgen":
+                    double a = sides[default(int)];
+                    double b = sides[1];
+                    double c = sides[2];
+                    return a + b + c;
+                case "kare":
+                    double side = sides[default(int)];
+                    return 4 * side;
+                case "dikdörtgen":
+                    double width = sides[default(int)];
+                    double height = sides[1];
+                    return 2 * (width + height);
+                default:
+                    throw new ArgumentException("Geçersiz geometrik şekil.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿ class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Geometrik şekli girin (Daire, Üçgen, Kare, Dikdörtgen): ");
+            string shape = Console.ReadLine();
+
+            Console.Write("Kenar bilgilerini virgülle ayırarak girin (Örnek: 2,3,4): ");
+            string sidesInput = Console.ReadLine();
+            double[] sides = Array.ConvertAll(sidesInput.Split(','), double.Parse);
+
+            Console.Write("Hesaplanmak istenen boyutu girin (Çevre, Alan): ");
+            string calculationType = Console.ReadLine();
+
+            if (calculationType.ToLower() == "alan")
+            {
+                double area = GeometricCalculator.GeometricCalculator.CalculateArea(shape, sides);
+                Console.WriteLine($"Sonuç: {area:F2}");
+            }
+            else if (calculationType.ToLower() == "çevre")
+            {
+                double perimeter = GeometricCalculator.GeometricCalculator.CalculatePerimeter(shape, sides);
+                Console.WriteLine($"Sonuç: {perimeter:F2}");
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz hesaplama tipi.");
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Geçersiz giriş.");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
